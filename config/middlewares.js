a778ca92e93425bb60d12db8d6d12828c972820c
n-a-n-a-k-i <nanaki.god@gmail.com>
2022-01-27 00:15:33 +0300

    update cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,17 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:1337',
+        'http://localhost:3000'
+      ]
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
